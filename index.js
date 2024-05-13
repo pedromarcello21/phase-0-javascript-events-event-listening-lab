9ac02a7bc707eaaf87de76338c1eb80e38b09024
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-05-13 09:45:14 -0400

    event listener practice

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-function addingEventListener() {
+const input = document.getElementById('button');
+
+const addingEventListener = () => {
+    input.addEventListener('click', addingEventListener)
+    alert('I was clicked! ')
 }
